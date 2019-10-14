d8d0eaaba36573f39c003f172c850dba80115ade
Vishal Tyagi <vishal.tyagi@sportshi.io>
2019-10-13 22:28:51 -0400

    Registering the created models in admin.py

--- coursebuilder/admin.py
+++ coursebuilder/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import CourseModule
+from .models import ModuleSection
+from .models import Quiz
+from .models import Question
+from .models import Extras
+from .models import Grade
 
 admin.site.register(CourseModule)
+admin.site.register(ModuleSection)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Extras)
+admin.site.register(Grade)
