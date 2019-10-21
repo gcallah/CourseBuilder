e99f96827bbc090867d61fc1ad62ec39fa4b61e8
Snehal Kenjale <snehalvk31@gmail.com>
2019-10-20 20:59:14 -0400

    Customized admin UI

--- coursebuilder/admin.py
+++ coursebuilder/admin.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
 
-from .models import CourseModule
-from .models import ModuleSection
-from .models import Quiz
-from .models import Question
-from .models import Extras
-from .models import Grade
+from .models import CourseModule, ModuleSection, Quiz,\
+    Question, Extras, Grade
 
-admin.site.register(CourseModule)
-admin.site.register(ModuleSection)
-admin.site.register(Quiz)
-admin.site.register(Question)
+
+class CourseModuleAdmin(admin.ModelAdmin):
+    list_display = ("module", "title", "course_order", "next_module")
+
+
+class ModuleSectionAdmin(admin.ModelAdmin):
+    list_display = ("title", "module", "order", "lesson_order")
+
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ("module", "numq", "minpass", "show_answers")
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("module", "text", "difficulty", "qtype", "correct", "answerA", "answerB", "answerC", "answerD", "answerE")
+
+
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ("quiz", "score", "record_date", "participant", "quiz_name")
+
+
+admin.site.register(CourseModule, CourseModuleAdmin)
+admin.site.register(ModuleSection, ModuleSectionAdmin)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Extras)
-admin.site.register(Grade)
+admin.site.register(Grade, GradeAdmin)
