e7b1298240ff29f93d9530cc2c95d7ed79a36649
gcallah <gcallah@mac.com>
2019-11-01 15:13:17 -0400

    Added rebuild.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # This shell file deploys a new version to our server.
 
+export project_name=CourseBuilder
+
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 echo "SSHing to PythonAnywhere."
-ssh coursebuildercourse@ssh.pythonanywhere.com << EOF
-    cd /home/coursebuildercourse/Onlinecoursebuilder
-    /home/coursebuildercourse/Onlinecoursebuilder/rebuild.sh
+ssh -o "StrictHostKeyChecking no" $project_name@ssh.pythonanywhere.com << EOF
+    cd ~/$project_name; ~/$project_name/rebuild.sh
 EOF
--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# for the prod server: fetches new code and restarts the server.
+# This runs on the production server: fetches new code,
+# installs needed packages, and restarts the server.
 
+export project_name=CourseBuilder
+
+# get new source code onto the server
 git pull origin master
-echo "going to reboot the webserver"
-touch /var/www/www_thecoursebuildercourse_com_wsgi.py
+# activate our virtual env:
+source /home/CourseBuilder/.virtualenvs/CourseBuilder/bin/activate
+# install all of our packages:
+pip install -r docker/requirements.txt
+echo "Going to reboot the webserver"
+API_TOKEN=10a10802d8fe9c6cce0181d30f0074fd6180cf5c pa_reload_webapp.py CourseBuilder.pythonanywhere.com
+touch reboot
