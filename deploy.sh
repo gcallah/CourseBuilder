3367de8679ea44a0b15fdde449b81ffa3b1cb8da
gcallah <gcallah@mac.com>
2019-11-08 15:43:16 -0500

    Changing options in deploy.sh -- experimenting.

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,8 @@ export project_name=CourseBuilder
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
 
 echo "SSHing to PythonAnywhere."
-ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking no" $project_name@ssh.pythonanywhere.com << EOF
+ssh -o "StrictHostKeyChecking no" $project_name@ssh.pythonanywhere.com << EOF
     cd ~/$project_name; ~/$project_name/rebuild.sh
 EOF
