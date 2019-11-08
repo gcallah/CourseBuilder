a0fd9dc9dbc55be586e46e268cfd463c7603b177
tirupal <rao.tirupal@gmail.com>
2019-11-07 20:15:45 -0500

    Fixed path error for key file

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,8 @@ chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
-ls ~/.ssh
 
 echo "SSHing to PythonAnywhere."
-ssh -o "StrictHostKeyChecking no" -i home/travis/.ssh/id_rsa $project_name@ssh.pythonanywhere.com << EOF
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa $project_name@ssh.pythonanywhere.com << EOF
     cd ~/$project_name; ~/$project_name/rebuild.sh
 EOF
