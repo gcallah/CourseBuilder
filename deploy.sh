9b5b3e29f60703dc59dfb67e67914ad7f355edcc
tirupal <rao.tirupal@gmail.com>
2019-11-07 20:50:47 -0500

    Moved ssh agent creation to before_install

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 
 export project_name=CourseBuilder
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
-ssh-add ~/.ssh/id_rsa # Add the private key to SSH
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
+#eval "$(ssh-agent -s)" # Start ssh-agent cache
+#chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
+#ssh-add ~/.ssh/id_rsa # Add the private key to SSH
+#chmod 700 ~/.ssh
+#chmod 600 ~/.ssh/authorized_keys
 
 echo "SSHing to PythonAnywhere."
 ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking no" $project_name@ssh.pythonanywhere.com << EOF
