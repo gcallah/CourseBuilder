4f089c5b04b07c1bb88f7f49a0ed67dd4b0175cb
tirupal <rao.tirupal@gmail.com>
2019-11-07 19:48:52 -0500

    Changed SSH command to use generated key

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,9 @@ export project_name=CourseBuilder
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
+echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 echo "SSHing to PythonAnywhere."
-ssh -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa $project_name@ssh.pythonanywhere.com << EOF
+ssh -i ~/.ssh/id_rsa $project_name@ssh.pythonanywhere.com << EOF
     cd ~/$project_name; ~/$project_name/rebuild.sh
 EOF
