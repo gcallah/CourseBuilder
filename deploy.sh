0cc4c09bdeabc32b9d914f4c9158bab645f655b4
tirupal <rao.tirupal@gmail.com>
2019-11-07 19:40:13 -0500

    Made changes in deploy.sh to attempt deployment

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 echo "SSHing to PythonAnywhere."
-ssh -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa $project_name@ssh.pythonanywhere.com pwd << EOF
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa $project_name@ssh.pythonanywhere.com << EOF
     cd ~/$project_name; ~/$project_name/rebuild.sh
 EOF
