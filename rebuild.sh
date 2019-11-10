b74f6d0ae3c6f59e0b0819172543ce394d01b5e6
TirupalRavila <rao.tirupal@gmail.com>
2019-11-10 10:56:27 -0500

    Added API token as a Travis env variable

--- rebuild.sh
+++ rebuild.sh
@@ -11,5 +11,5 @@ source /home/CourseBuilder/.virtualenvs/CourseBuilder/bin/activate
 # install all of our packages:
 pip install -r docker/requirements.txt
 echo "Going to reboot the webserver"
-API_TOKEN=10a10802d8fe9c6cce0181d30f0074fd6180cf5c pa_reload_webapp.py CourseBuilder.pythonanywhere.com
+API_TOKEN=$1 pa_reload_webapp.py CourseBuilder.pythonanywhere.com
 touch reboot
