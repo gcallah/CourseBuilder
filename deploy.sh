ab4711c6d0e38a4c5f51bd40165105b44ce192cb
TirupalRavila <rao.tirupal@gmail.com>
2019-11-10 01:30:02 -0500

    Removed SSH agent related script

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,6 @@
 
 export project_name=CourseBuilder
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
-ssh-add ~/.ssh/id_rsa # Add the private key to SSH
-
 echo "SSHing to PythonAnywhere."
 sshpass -p $1 ssh -o "StrictHostKeyChecking no" $project_name@ssh.pythonanywhere.com << EOF
     cd ~/$project_name; ~/$project_name/rebuild.sh
